0905d64e4506406a495bbb972dffc4fc6361ebcc
Florence Hapgood <flohapgood@icloud.com>
2019-01-12 21:28:07 +0000

    App renders navbar, counters and counter components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class NavBar extends Component {
-  render = () {
+  render() {
     return (
       <nav className="navbar navbar-light bg-light">
         <a class="navbar-brand" href="#">
